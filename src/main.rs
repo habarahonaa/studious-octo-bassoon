504ccf04971d69bfeb6dac50693a5c3f97866d0c
Hollman A. Barahona <habarahonaa@gmail.com>
2022-07-28 22:00:12 -0600

    feat: adds standalone binary and vga buffer controller (wip)

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
+
+/// This function is called on panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
